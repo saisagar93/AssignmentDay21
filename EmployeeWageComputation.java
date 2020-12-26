32b675f1518ccba3069f844b55617c246322200c
saisagar93 <sscreator225@gmail.com>
2020-12-26 14:26:15 +0530

    Add:- code to Calculate wages using method

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,28 +1,25 @@
 public class EmployeeWageComputation
 {
-
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int MAX_WORKING_HRS=100;
 
-	public static void main(String[] args)
+	public static int computeEmpwage()
 	{
-
 		System.out.println("Welcome to employee wage computation");
 		int empHrs=0;
 		int totalEmpWage=0;
 		int empWage=0;
 		int totalEmpHrs=0;
-		int totalWorkingDays=0;	
-		//while loop for less than 20 working days and less than 100 working hours
+		int totalWorkingDays=0;
 		while(totalEmpHrs<=MAX_WORKING_HRS && totalWorkingDays<=NUM_OF_WORKING_DAYS)
 		{
 			totalWorkingDays++;
-			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;	//To generate a random valueof 0 to 2
 
-			switch(empCheck)
+			switch(empCheck)					//switch case for employee fulltime,parttime and absent
 			{
 				case FULL_TIME:
 				empHrs=8;
@@ -35,9 +32,14 @@ public class EmployeeWageComputation
 				default:
 				empHrs=0;
 			}
-			totalEmpHrs+=empHrs;
+			totalEmpHrs+=empHrs;					//Total working hours
 		}
-		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
+		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;                       //Salary of an employee
 		System.out.println("Total Employee Wage: "+totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args)
+	{
+		computeEmpwage();
 	}
 }
