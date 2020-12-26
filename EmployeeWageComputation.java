f3c980a089d55bb5dbeda1b453c54969857108c0
saisagar93 <sscreator225@gmail.com>
2020-12-26 14:13:46 +0530

    Add:- code to Calculate wages of Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,6 +1,5 @@
 public class EmployeeWageComputation
 {
-
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
@@ -10,25 +9,32 @@ public class EmployeeWageComputation
 	{
 
 		System.out.println("Welcome to employee wage computation");
-		//variables
 		int empHrs=0;
-		int salary=0;
-		int empCheck=(int)Math.floor(Math.random()*10)%3;		//To generate a random valueof 0 to 2
+		int totalEmpWage=0;
+		int empWage=0;
 
-		switch(empCheck)						//switch case for employee fulltime,parttime and absent 
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)			//for loop for 20 working days
 		{
-			case FULL_TIME:
-			empHrs=8;
-			break;
+			int empCheck=(int)Math.floor(Math.random()*10)%3;	//To generate a random valueof 0 to 2
 
-			case PART_TIME:
-			empHrs=4;
-			break;
+			switch(empCheck)					//switch case for employee fulltime,parttime and absent 
+			{
+				case FULL_TIME:
+				empHrs=8;
+				break;
 
-			default:
-			empHrs=0;
+				case PART_TIME:
+				empHrs=4;
+				break;
+
+                        	default:
+				empHrs=0;
+			}
+			empWage=empHrs*EMP_RATE_PER_HR;				//salary of an employee for a day
+			totalEmpWage+=empWage;
 		}
-		salary=empHrs*EMP_RATE_PER_HR;					//salary of an employee
-                System.out.println("Employee Wage: "+salary);
+
+		System.out.println("Total Employee Wage: "+totalEmpWage);	//salary for a month
 	}
 }
+
