3e9b0990ace0019f8e468d7ede9ed4dae8b31196
saisagar93 <sscreator225@gmail.com>
2020-12-26 12:51:16 +0530

    Employee wage computation

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+public class EmployeeWageComputation						//Main class
+{
+	public static void main(String[] args)					//Main method
+	{
+		System.out.println("Welcome to employee wage computation");
+
+	}
+}
